3e46c8dde5115a7aa5f8070131a9e8cbe4471923
Caroline Barbosa <bo.varig@gmail.com>
2022-07-01 22:10:56 -0300

    created the second possible code

--- module-js-palindromo/assets/js/script.js
+++ module-js-palindromo/assets/js/script.js
@@ -3,3 +3,15 @@ function verifiedPalindrom(string) {
 
 	return string.split('').reverse().join('') === string;
 }
+function verifiedPalindrom2(string) {
+  if(!string) return "string doesn't exists";
+
+  for(let i =0; i <string.length / 2; i++) {
+    if(string[i] !== string[string.length - 1 - i]){
+    return false;
+    }
+  }
+
+  return true;
+}
+console.log(verifiedPalindrom2('abba'));
